cb02c246da037093177eeef2471c0e5dd0c3bb8c
demezalan <zalan.deme@student.uni-miskolc.hu>
2024-04-22 13:28:30 +0200

    Félig elkészült 1. feladat

---
+++ NDZDH6_0422/NDZDH6_1.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+   console.log('NDZDH6_0422/NDZDH6_1.js');
+});
+
+$("button#animation").click(() => {
+    $("div#rect")
+});
+$("button#hide").click(() => {
+    $("div#paragraphs").hide();
+    alert("Bekezdések elrejtése");
+});
+$("button#collapseOpen").click(() => {
+    $("p.hide").toggle();
+});
+
