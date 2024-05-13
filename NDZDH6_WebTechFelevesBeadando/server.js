58dde272f8ee68bd499ea52b0996cc5a900fb7bf
demezalan <zalan.deme@student.uni-miskolc.hu>
2024-05-13 12:10:53 +0200

    Hibakezelés nodeJS-ben

--- NDZDH6_WebTechFelevesBeadando/server.js
+++ NDZDH6_WebTechFelevesBeadando/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,35 +13,43 @@ app.get('/', (req, res) => {
 });
 
 app.post("/form.html", (req, res) => {
-
-  const fs = require('fs');
   const formData = req.body;
   const jsonString = JSON.stringify(formData, null, 2);
 
-  fs.readFile('answers.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      return;
-    }
-
-    let existingData = [];
-
-    if (data.trim() !== '') {
-      existingData = JSON.parse(data);
-    }
-
-    existingData.push(formData);
-    const updatedJsonString = JSON.stringify(existingData, null, 2);
-
-    fs.writeFile('answers.json', updatedJsonString, (err) => {
+  try {
+    fs.readFile('answers.json', 'utf8', (err, data) => {
       if (err) {
-        console.error('Error writing file:', err);
-      } else {
-        console.log('A new submit has been recorded in answers.json');
+        console.error('Error reading file:', err);
+        return res.status(500).send('Error reading file');
       }
+
+      let existingData = [];
+
+      if (data.trim() !== '') {
+        existingData = JSON.parse(data);
+      }
+
+      existingData.push(formData);
+      const updatedJsonString = JSON.stringify(existingData, null, 2);
+
+      fs.writeFile('answers.json', updatedJsonString, (err) => {
+        if (err) {
+          console.error('Error writing file:', err);
+          return res.status(500).send('Error writing file');
+        }
+        console.log('A new submit has been recorded in answers.json');
+        res.redirect('/');
+      });
     });
-  });
-  res.redirect('/');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('There was an error.');
 });
 
 app.listen(PORT, () => {
